c67f82aa6d3e1260d0b74dddc74d41baa37232d9
praran <pradeep.muralidharan@gmail.com>
2016-01-21 11:57:33 +0000

    making heroku ready

--- src/main/java/com/develogical/WebServer.java
+++ src/main/java/com/develogical/WebServer.java
@@ -17,7 +17,8 @@ import java.io.IOException;
 public class WebServer {
 
     public WebServer() throws Exception {
-        Server server = new Server(8080);
+        int port = Integer.valueOf(System.getenv("PORT"));
+        Server server = new Server(port);
 
         ServletHandler handler = new ServletHandler();
         handler.addServletWithMapping(new ServletHolder(new Website()), "/*");
